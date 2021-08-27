9de4819f965be1833c807c254b351e074fbfe64c
dsbaek2020 <60423547+dsbaek2020@users.noreply.github.com>
2021-08-27 14:01:12 +0900

    Update Final_uartHpmParticleSensor.cpp

--- weatherstationRPi/src/Final_uartHpmParticleSensor.cpp
+++ weatherstationRPi/src/Final_uartHpmParticleSensor.cpp
@@ -1,3 +1,9 @@
+//Author : ChoiSY
+//2021.8.27(금)
+
+
+
+
 #include<stdio.h>
 #include<fcntl.h>
 #include<unistd.h> //for sleep function
@@ -15,8 +21,8 @@
 //#define  LED_GPIO    17
 
 
-//센서와 통신하기 위한 명령 및 응답 데이터 자료구조 정의 
-//명령 
+//센서와 통신하기 위한 명령 및 응답 데이터 자료구조 정의
+//명령
 const char readResultCmd[4]      = {0x68,0x01,0x04,0x93};
 const char stopAutoSendCmd[4]    = {0x68,0x01,0x20,0x77};
 const char enableAutoSendCmd[4]  = {0x68,0x01,0x40,0x57};
@@ -24,11 +30,11 @@ const char stopParticleMes[4]    = {0x68,0x01,0x02,0x95};
 
 //응답(구조체)
 typedef struct response{
-	   char head;
-	   char len;
-	   char cmd;
-	   char dataField[4];
-	   char cs;
+	   char head;  //0
+	   char len;   //1
+	   char cmd;   //2
+	   char dataField[4];  //3 4 5 6
+	   char cs;            // 7
 }t_response;
 
 //응답(구조체 공용체)
@@ -36,6 +42,14 @@ typedef union rxSensorResponse{
 	   char rxByte[8];
 	   t_response resp;
 }t_rxSensorResponse;
+
+
+enum responseRxStatus{
+	Ready,
+	Head,
+	Length,
+	CMD
+};
 
 
 
@@ -95,60 +109,105 @@ int main(int argc, char *argv[]){
    }while(read(client,&c,1)>0);
    printf("\n");
 
-   //열번만 센서값을 읽어와서 전시해 본다. 
+   //열번만 센서값을 읽어와서 전시해 본다.
    int k = 0;
-   while(k<10){
+   while(1){
       //Send SensorReadCommand from RPi to Sensor with UART(ttyUSB0).
       if(write(client, readResultCmd, sizeof(readResultCmd))<0){
         perror("Error: Failed to write to the sensor\n");
-        return -1; 
+        return -1;
       }
 
-      //단순한 수신 알고리즘 ---- (최종적으로는 사용하면 안됨) 
-      if(read(client,&rxSensorResponse.rxByte,8)==8){
-        PM10 = rxSensorResponse.resp.dataField[2]*256 + rxSensorResponse.resp.dataField[3];
-        PM25 = rxSensorResponse.resp.dataField[0]*256 + rxSensorResponse.resp.dataField[1];
-        printf("PM10 = %d, PM25 = %d \n", PM10, PM25);
-        
-        for(int i=0; i<8; i++){
-          printf("[%x] ", rxSensorResponse.rxByte[i]); 
-        }
-        printf("\n");
-      }
-     
+
      //센서 데이터 수신 알고리즘 작성 부분 (센서 전원이 중간에 다시켜지는 상황까지 고려한 알고리즘)------------
-       //여기에 코딩하세요 
+       //여기에 코딩하세요
+
+      char readData;
+      enum responseRxStatus rxStatus;
+      rxStatus = Ready;
+      char errorCount =0;
+
+      do{
+    	  if(read(client,&readData,1)>0){
+
+    		  if(rxStatus == Ready && readData == 0x40){
+    			  rxStatus = Head;
+    			  rxSensorResponse.resp.head = readData;
+
+    		  }
+    		  else if(rxStatus == Head && readData == 0x05){
+    			  rxStatus = Length;
+    			  rxSensorResponse.resp.len = readData;
+    		  }
+    		  else if(rxStatus == Length && readData == 0x04){
+    		      rxStatus = CMD;
+    		      rxSensorResponse.resp.cmd = readData;
+    		  }
+    		  else{
+    			  rxStatus = Ready;
+    			  rxSensorResponse.rxByte[0] = 0;
+    			  rxSensorResponse.rxByte[1] = 0;
+    			  rxSensorResponse.rxByte[2] = 0;
+    			  errorCount++;
+    		  }
+
+    	  }
+    	  else{
+    		  errorCount++;
+
+    		  //에러를 카운트 한다. 만약 에러가 100개 넘으면 break 해서 나온다.
+    		  if(errorCount == 10){
+    			  break;
+    		  }
+    	  }
+
+      }while(rxStatus != CMD);
+
+      if(rxStatus == CMD){
+		if(read(client,&rxSensorResponse.resp.dataField,5)==5){
+			PM10 = rxSensorResponse.resp.dataField[2]*256 + rxSensorResponse.resp.dataField[3];
+			PM25 = rxSensorResponse.resp.dataField[0]*256 + rxSensorResponse.resp.dataField[1];
+			printf("PM10 = %d, PM25 = %d \n", PM10, PM25);
+
+			/*or(int i=0; i<8; i++){
+				printf("[%x] ", rxSensorResponse.rxByte[i]);
+			}*/
+			//printf("\n");
+		}
+      }
+
+
      // -------------------------------------------------------------------------------------
-     
-     
+
+
 	  usleep(100000); //100ms  //센서 데이터 받는 주기를 0.1초마다 수행하기 위해서 딜레이
 	  //sleep(1);
       k++;
    }
 
-    //센서를 정지 시킨다. 
+    //센서를 정지 시킨다.
     if(write(client, stopParticleMes,4)<0){
     perror("Error : Failed to write to the sensor\n");
-    return -1; 
+    return -1;
     }
-    
+
     //정지가 되었는지 확인한다. NOTE ***** (디버깅 필요함, 데이터가 0x40, 0x05가 계속 수신됨)  << NOTE *****
     char response[2];
     if(read(client,&response,2)==2){
-      
+
       printf("[%x], [%x] ", response[0], response[1]);
-      
+
       if((response[0]==0xa5) && (response[1]==0xa5)){
         printf("closed!!!!!! \n ");
       }else{
-        printf("Bad closed \n"); 
-      } 
+        printf("Bad closed \n");
+      }
     }
-  
+
    close(client);
    return 0;
 
- 
+
 }
 
 
